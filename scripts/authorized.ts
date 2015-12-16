dd2c6c37c06144267a963bdd6c863830ac8ba8de
yao <yao@boqii.com>
2015-12-16 16:06:43 +0800

    get authorized application

---
+++ scripts/authorized.ts
@@ -0,0 +1,69 @@
+import * as base from "./base";
+import * as common from "./common";
+import * as types from "./types";
+
+declare let Vue;
+
+interface VueBodyModel {
+    applications: types.Application[];
+    application: types.Application;
+
+    show: (application: types.Application) => void;
+    get: () => void;
+    revoke: (application: types.Application) => void;
+}
+
+let vueBody: VueBodyModel = new Vue({
+    el: "#vue-body",
+    data: {
+        applications: [],
+        application: null,
+    },
+    methods: {
+        show: function(application: types.Application) {
+            let self: VueBodyModel = this;
+
+            self.application = application;
+        },
+        get: function() {
+            let self: VueBodyModel = this;
+
+            $.ajax({
+                url: base.apiUrl + "/api/user/authorized",
+                cache: false,
+            }).then((data: types.ApplicationsResponse) => {
+                if (data.isSuccess) {
+                    self.applications = data.applications;
+                } else {
+                    base.vueHead.showAlert(false, data.errorMessage);
+                }
+            });
+        },
+        revoke: function(application: types.Application) {
+            let self: VueBodyModel = this;
+
+            $.ajax({
+                url: base.apiUrl + `/api/user/authorized/${application.id}`,
+                method: "DELETE",
+            }).then((data: types.Response) => {
+                if (data.isSuccess) {
+                    base.vueHead.showAlert(true, "success");
+                    self.application = null;
+                    self.get();
+                } else {
+                    base.vueHead.showAlert(false, data.errorMessage);
+                }
+            });
+        },
+    },
+});
+
+$(document).ready(function() {
+    base.vueHead.authenticate(error => {
+        if (error) {
+            console.log(error);
+        }
+
+        vueBody.get();
+    });
+});
